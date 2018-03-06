f1dc1bcc641ad2a9c56916e4cf4878e2df716be3
Yao Pei <scpeiyao@gmail.com>
2018-03-06 12:15:07 +0800

    test survey

---
+++ test_survey.py
@@ -0,0 +1,11 @@
+import unittest
+from survey import AnonymousSurvey
+class TestAnonmyousSurvey(unittest.TestCase):
+    """Test for the class AnonymousSurvey"""
+    def test_store_single_response(self):
+        """Test that a single response is stored properly."""
+        question='What language did you first learn to speak?'
+        my_survey=AnonymousSurvey(question)
+        my_survey.store_response('English')
+        self.assertIn('English',my_survey.responses)
+    unittest.main()
